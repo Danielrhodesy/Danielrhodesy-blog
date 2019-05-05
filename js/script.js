0bbc31f549e9bb322e492fb79608f62948432f87
Daniel Rhodes <danielrhodesnz@gmail.com>
2019-05-06 08:32:12 +1200

    added 4th blog - DOM

--- js/script.js
+++ js/script.js
@@ -39,13 +39,14 @@ function animsitionCode (blogNum, newPage, newPageGrid, gridsize){
 animsitionCode('#blog1id', '.blog1-page', '.blog1-page-grid', '70vw')
 animsitionCode('#blog2id', '.blog2-page', '.blog2-page-grid', '115vw')
 animsitionCode('#blog3id', '.blog3-page', '.blog3-page-grid', '110vw')
+animsitionCode('#blog4id', '.blog4-page', '.blog4-page-grid', '110vw')
 
 
 function backAnimsitionCode (backbtn, oldpage){
     $(backbtn).click(function(){
         $(oldpage).fadeOut(function(){
             $('.grid-container-main').hide(function(){
-                $('.grid-container-size').css("height", "50vw")
+                $('.grid-container-size').css("height", "auto")
                 $('.main').fadeIn(400)
               
                
@@ -57,6 +58,7 @@ function backAnimsitionCode (backbtn, oldpage){
 backAnimsitionCode('#backButton','.blog1-page')
 backAnimsitionCode('#backButton2', '.blog2-page')
 backAnimsitionCode('#backButton3', '.blog3-page')
+backAnimsitionCode('#backButton4', '.blog4-page')
 
 
 
