3f73d91fd1f6f82c901fd8d66b592ae8c0e2677c
Daniel Rhodes <danielrhodesnz@gmail.com>
2019-05-12 20:03:20 +1200

    Tech blog/cultural blog in

--- js/script.js
+++ js/script.js
@@ -40,7 +40,8 @@ animsitionCode('#blog1id', '.blog1-page', '.blog1-page-grid', '70vw')
 animsitionCode('#blog2id', '.blog2-page', '.blog2-page-grid', '115vw')
 animsitionCode('#blog3id', '.blog3-page', '.blog3-page-grid', '110vw')
 animsitionCode('#blog4id', '.blog4-page', '.blog4-page-grid', '110vw')
-
+animsitionCode('#blog5id', '.blog5-page', '.blog5-page-grid', '110vw')
+animsitionCode('#blog6id', '.blog6-page', '.blog6-page-grid', '180vw')
 
 function backAnimsitionCode (backbtn, oldpage){
     $(backbtn).click(function(){
@@ -59,28 +60,8 @@ backAnimsitionCode('#backButton','.blog1-page')
 backAnimsitionCode('#backButton2', '.blog2-page')
 backAnimsitionCode('#backButton3', '.blog3-page')
 backAnimsitionCode('#backButton4', '.blog4-page')
-
-
-
-
-//   function animsitionCode (clicked, divA, divB, divC) {
-//     $(clicked).click(function(){
-//       $(divA).fadeOut(function(){
-//         $(divB).hide(function(){
-//           $(divC).fadeIn(400, function(){
-
-//           createMap()
-
-//           });
-//         });
-//       });
-//     });
-//   };
-
-// animsitionCode('.here-button', '#mainpage-div', '#mainpage-div', '#page2-div')
-// animsitionCode('.ph-button', '#page2-div', '#page2-div', '#page3-div')
-// animsitionCode('.m-button', '#page4-div', '#page4-div', '#page5-div')
-
+backAnimsitionCode('#backButton5', '.blog5-page')
+backAnimsitionCode('#backButton6', '.blog6-page')
 
 
 
